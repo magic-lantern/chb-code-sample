72023937ee75cd43868f02d1944af9d6e5ffe671
magic-lantern <magic-lantern@users.noreply.github.com>
2017-03-08 23:48:21 -0700

    Updated documentation. Implemented REST call to get applications

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 ## Application Requirements
-Create a single route, unauthenticated web service in Node.js or Python to return a JSON array of app gallery listings created or updated on/after a specified date. The user should be able to pass in the target date as a querystring parameter (or no date to retrieve all listings in the db). The data and schema are in the attached Postgres SQL dump and the “updated_at” date is in the applications table. All of the tables will need to be joined to generate the full response and only applications with a status of “published” in the listings table should be returned. Bonus points for implementing paging, ip based throttling and other standard API features as time allows. Functionality that is designed but not implemented can be indicated through failing unit tests or placeholder comments in the code.
+Create a single route, unauthenticated web service in Node.js or Python to return a JSON array of app gallery listings created or updated on/after a specified date. The user should be able to pass in the target date as a querystring parameter (or no date to retrieve all listings in the db). The data and schema are in the Postgres SQL dump file and the “updated_at” date is in the applications table. All of the tables will need to be joined to generate the full response and only applications with a status of “published” in the listings table should be returned. Bonus points for implementing paging, ip based throttling and other standard API features as time allows. Functionality that is designed but not implemented can be indicated through failing unit tests or placeholder comments in the code.
+
+**Specifically, this means:**
+1. No authentication required web service
+1. Target date passed in as a query string parameter
+1. No date returns all results
+1. Only applications with a status of “published” in the listings table should be returned
+
+**Assumptions Made:**
+1. Incorrect URL parameters names will result in an error
+1. Results are in descending order by updated_at
+1. Invalid dates are treated as if no date passed.
 
 ## Additional Features Needed
 * Database query throttling - store result, don't re-query db if too soon
@@ -8,8 +19,7 @@ Create a single route, unauthenticated web service in Node.js or Python to retur
  * page parameter in URL. If missing, then get first page automatically
  * Response should indicate page X of Y or nextrecords. Something like page?limit=5&start=10
 
-
-## How to setup and run applciationSetup
+## How to setup and run applciation
 ### PostgreSQL
 pg_dump file doesn't create database:
 
@@ -31,3 +41,16 @@ Additionally, in order to have the administrative password be something you know
     conda create --name smart python=3.5
     source activate smart
     conda list --export
+
+## Developer notes
+Since I'm using SQLAlchemy automap to generate classes from database introspection, it appears that:
+1. I cannot use Flask's paginate() function. http://stackoverflow.com/questions/18468887/flask-sqlalchemy-pagination-error
+1. Can't seem to get table relationships to work automatically. Possibly could override automapped class, but found a decent solution.
+ * http://stackoverflow.com/questions/22019567/how-to-get-flask-sqlalchemy-object-to-load-relationship-children-for-jinja-templ
+ * http://stackoverflow.com/questions/12698766/how-to-load-nested-relationships-in-sqlalchemy
+ * http://docs.sqlalchemy.org/en/latest/orm/loading_relationships.html#sqlalchemy.orm.subqueryload
+1. Alternative methods for converting SQLAlchemy results to JSON: http://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
+1. REST APIs with pagination
+ * https://developer.atlassian.com/confdev/confluence-server-rest-api/pagination-in-the-rest-api
+ * http://stackoverflow.com/questions/13872273/api-pagination-best-practices
+ * https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ix-pagination
--- app_gallery.py
+++ app_gallery.py
@@ -1,6 +1,8 @@
-from flask import Flask, got_request_exception
-from flask_restful import Resource, Api, fields, marshal_with, reqparse
-
+from flask import Flask, got_request_exception, jsonify, request
+from flask_restful import Api, reqparse
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+from dateutil.parser import parse
 
 ############################
 # Application wide variables
@@ -8,17 +10,151 @@ app = Flask(__name__)
 app.config['BUNDLE_ERRORS'] = True
 api = Api(app)
 url_parameters = ['date', 'page']
-# connect to database
+# value intentionally set low for easy testing
+PAGE_SIZE = 2
+# value intentionally set low for easy testing
+MAX_REQUESTS = 5
 
+# connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:32771/gallery'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+automapper = automap_base()
+automapper.prepare(db.engine, reflect=True)
 ############################
 
-
+# Standard hello world response to let people know what this application does
 @app.route('/')
 def hello_world():
     return '<html><body>' \
            + '<p>JSON API RESTFUL URL available at <a href="/gallery">/gallery</a></p>' \
+           + '<p>Returns JSON array of all SMART applications available</p>' \
+           + '<p>Optional parameters:<ul>' \
+           + '<li>date - Only applications updated on or newer than date provided will be returned.' \
+           + ' Date format must be valid according to <a href="https://dateutil.readthedocs.io/en/stable/parser.html">https://dateutil.readthedocs.io/en/stable/parser.html</a></li>' \
+           + '<li>page - NOT YET IMPLEMENTED - If more than ' + str(PAGE_SIZE) + ' records are found, can view additional records via page parameter</li>' \
+           + '</ul></p>' \
            + '</body></html>'
 
+hit_counter = {}
+@app.route('/gallery')
+def gallery_query():
+    # TO DO: need to implement more intelligent rate limiting
+    # if too many requests from one IP return nothing, or error?
+    # if memoization implemented, perhaps this should be tied with memoization?
+    print(hit_counter)
+    if request.remote_addr not in hit_counter:
+        hit_counter[request.remote_addr] = 1
+    else:
+        hit_counter[request.remote_addr] += 1
+    if hit_counter[request.remote_addr] > MAX_REQUESTS:
+        print('too many visits -------------------------')
+        return ('', 204)
+
+    applications = automapper.classes['applications']
+    listings = automapper.classes['listings']
+
+    # get request parameters
+    get_parser = reqparse.RequestParser()
+    for u in url_parameters:
+        get_parser.add_argument(
+            u,
+            dest = u,
+            help = u + ' parameter',
+        )
+
+    # throw error on unrecognized parameters - this may not be wanted...
+    args = get_parser.parse_args(strict=True)
+
+    query = db.session.query(applications).join(listings).filter(listings.status == 'published').order_by(applications.updated_at.desc())
+
+    try:
+        query = query.filter(applications.updated_at >= parse(args['date']))
+    except(ValueError, TypeError):
+        print("Date parameter not valid or missing")
+
+    # TO DO implement paging of results
+    if args['page'] is not None:
+        try:
+            page = int(args['page'])
+        except ValueError:
+            print("can't convert")
+        else:
+            # paginate results here - replace this line
+            result = query.all()
+    else:
+        result = query.all()
+
+    # output for debugging purposes
+    print(query)
+
+    # build return object - start with applications table as base and add others
+    retarr = []
+    if result is not None:
+        print(result)
+        for record in result:
+            ret = {}
+            for c in applications.__table__.columns:
+                ret[c.name] = getattr(record, c.name)
+            ret = populate_collections(ret)
+            retarr.append(ret)
+
+    return jsonify(retarr)
+
+############################
+# function to populate the various subcollections associated with an application
+def populate_collections(result_obj):
+    applications_fhir = automapper.classes['applications_fhir']
+    fhir = automapper.classes['fhir']
+    collections = [{
+        'src' : automapper.classes['applications_categories'],
+        'dest' : automapper.classes['categories'],
+        'name' : 'applications_categories'
+    }, {
+        'src' : automapper.classes['applications_ehr'],
+        'dest' : automapper.classes['ehr'],
+        'name' : 'applications_ehr'
+    }, {
+        'src' : automapper.classes['applications_fhir'],
+        'dest' : automapper.classes['fhir'],
+        'name' : 'applications_fhir'
+    }, {
+        'src' : automapper.classes['applications_operating_systems'],
+        'dest' : automapper.classes['operating_systems'],
+        'name' : 'applications_operating_systems'
+    }, {
+        'src' : automapper.classes['applications_pricing'],
+        'dest' : automapper.classes['pricing'],
+        'name' : 'applications_pricing'
+    }, {
+        'src' : automapper.classes['applications_specialties'],
+        'dest' : automapper.classes['specialties'],
+        'name' : 'applications_specialties'
+    }]
+
+    for c in collections:
+        results = db.session.query(c['src']).join(c['dest']).filter(c['src'].application_id == result_obj['id']).add_column(c['dest'].name).all()
+        result_obj[c['name']] = []
+        for record in results:
+            result_obj[c['name']].append(record.name)
+
+    return result_obj
+
+############################
+# need to use this in gallery_query()
+# function not validated to work correctly
+memory = {}
+MEMORY_AGE = 120
+def memoize(table, params):
+    if memory[table][params] is None:
+        memory[table][params] = run_query(table, params)
+    elif memory[table][params][stored] >= (time.time() - MEMORY_AGE):
+        memory[table][params] = run_query(table, params)
+    else:
+        return memory[table][params]
+
+############################
+# exception logging
 def log_exception(sender, exception, **extra):
     """ Log an exception to our logging framework """
     sender.logger.debug('Got exception during processing: %s', exception)
@@ -35,5 +171,7 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET')
     return response
 
+############################
+# start application in debug mode, also listen on all ip addresses
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
